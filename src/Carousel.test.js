286a8bef8ea3bb4612402568659d5936944e51e0
JJ <jonjones320@gmail.com>
2024-07-18 01:52:11 -0700

    Added smoke test for Carousel component.

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -1,6 +1,13 @@
+import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
+
+
+it("renders without crashing", function() {
+  render(<Carousel />);
+});
+
 
 it("works when you click on the right arrow", function() {
   const { container } = render(
